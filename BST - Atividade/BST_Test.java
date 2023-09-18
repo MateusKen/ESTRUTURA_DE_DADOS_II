a933ca78f8fe298ca9718271aac52bb409a86574
Erik Hsu <111096802+erikhsu08@users.noreply.github.com>
2023-09-18 17:33:26 -0300

    Update BST_Test.java

--- BST - Atividade/BST_Test.java
+++ BST - Atividade/BST_Test.java
@@ -1,60 +1,85 @@
 package atvBST;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class BST_Test {
-
-	public static void main(String[] args) {		
-
-		BST bst = new BST();
-		Scanner scanner = new Scanner(System.in);
-
-		while (true) {
-			
-			System.out.println("\n\n\nBinary Search Tree Test Menu:");
-			System.out.println("1. Insert");
-			System.out.println("2. Delete");
-			System.out.println("3. Search");
-			System.out.println("4. Inorder Traversal");	
-			System.out.println("5. Number of Nodes");
-			System.out.println("6. Exit");
-			System.out.print("Enter your choice: ");
-			
-			String choiceStr = scanner.nextLine();
-			int choice = Integer.parseInt(choiceStr);
-			String data = "";
-			switch (choice) {
-			case 1:
-				System.out.print("Enter data to insert: ");
-				data = scanner.nextLine();
-				bst.insert(data);
-				break;
-			case 2:
-				System.out.print("Enter data to delete: ");
-				data = scanner.nextLine();
-				bst.remove(data);
-				break;
-			case 3:
-				System.out.print("Enter data to search: ");
-				data = scanner.nextLine();
-				boolean found = 
-						(bst.search(data) != null) ? true: false;
-				System.out.println("Value " + data + " found: " + found);
-				break;
-			case 4:
-				System.out.println("InOrder Traversal:");
-				String resultado = bst.inOrderTraversal();
-				System.out.println(resultado);
-				break;
-			case 5:
-				System.out.println("Number of nodes:"+bst.countNodes());
-				break;				
-			case 6:
-				System.out.println("Exiting...");
-				scanner.close();
-				System.exit(0);
-			default:
-				System.out.println("Invalid choice. Please enter a valid option.");
-			}
-		}
+		//--------------- Métodos implementados -------------------
+    
+	//Gastos - salários dos funcionários
+	
+	//Total de funcionários de um determinado sexo
+	public static int contSexo(BST bst, char escolhaSexo) {
+		int contSexo = 0;
+		List<Funcionario> todosFuncionarios = bst.inOrderTraversal();
+        for (Funcionario funcionario : todosFuncionarios) {
+        	if (funcionario.getSexo() == escolhaSexo){
+        		contSexo++;
+        	}
+        }
+        return contSexo;
 	}
-}
+	//Total de funcionários de uma determinada categoria
+
+	//Mostrar os dados dos funcionários com idade superior a uma determinada idade
+
+	//main
+    public static void main(String[] args) {
+    	//Criar uma BST
+        BST bst = new BST();
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        
+        //-------------- Valores de entrada ----------------
+        //Criar funcionários
+        Funcionario func1 = new Funcionario(1, 20, 'P', 'M', "Joao", "Desenvolvedor", 5000);
+        Funcionario func2 = new Funcionario(2, 24, 'O', 'F', "Fabiana", "Consultora", 3000);
+        Funcionario func3 = new Funcionario(3, 74, 'H', 'M', "Paulo", "Analista", 8000);
+        
+        //-------------- Inserir os funcionarios na arvore --------------
+        bst.insert(func1);
+        bst.insert(func2);
+        bst.insert(func3);
+        
+        //-------------- Menu de opções ---------------------
+        
+        int opcao = 0;
+        
+        do {
+        	
+        	System.out.println("Menu de opções da BST: ");
+            System.out.println("1. Mostrar resultado dos gastos com os salários dos funcionários");
+            System.out.println("2. Mostrar o total de funcionários de um determinado sexo.");
+            System.out.println("3. Mostrar o total de funcionários de uma determinada categoria.");
+            System.out.println("4. Mostrar os dados dos funcionários com idade superior a uma determinada idade informada.");
+            System.out.println("5. Encerrar o programa e o menu de opções.");
+            System.out.print("Digite sua opção: ");
+            	
+            opcao = scanner.nextInt();
+            switch(opcao) {
+            case 1:
+            	//implementar
+            	break;
+            case 2:
+            	System.out.println("Você deseja saber a quantidade de funcionários do sexo:");
+            	System.out.println("Masculino - tecle M \nFeminino - tecle F");
+            	char escolhaSexo = scanner.next().charAt(0);
+            	int qntFuncionarios = contSexo(bst, escolhaSexo);
+            	System.out.println("Número de funcionários do sexo " + escolhaSexo + ": " + qntFuncionarios);
+            	System.out.println("\n");
+            	break;
+            case 3:
+            	//implementar
+            	break;
+            case 4:
+            	//implementar
+            	break;
+            case 5:
+            	System.out.println("Programa encerrado");
+            	scanner.close();
+            	break;	
+            } // fim do switch
+        }while (opcao != 5);
+   }
+} // Fim da classe BST_Test.java
